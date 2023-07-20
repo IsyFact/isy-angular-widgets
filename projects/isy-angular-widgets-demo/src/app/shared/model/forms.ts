8c3b18e471246450491b68bc4445b10514e99d5f
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-07-20 15:03:51 +0200

    feat: Added Interface for casting Form sub-object

---
+++ projects/isy-angular-widgets-demo/src/app/shared/model/forms.ts
@@ -0,0 +1,14 @@
+export interface PersonalInformation {
+  lastName: string;
+  birthName: string;
+  birthplace: string;
+  firstName: string;
+  gender: string;
+  birthDate: string;
+  nationality: string;
+  phoneNumber: string;
+  dateOfEntry: string;
+  idRequired: string;
+  securityLevel: string;
+  intelligenceNotes: string;
+}
