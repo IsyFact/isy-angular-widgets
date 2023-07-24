5f476d813dd9c60cf6deae0247d6295332e62607
braatt <tim.braatz@msg.group>
2023-07-24 16:41:33 +0200

    test: repaired testing module

--- projects/isy-angular-widgets-demo/src/app/feature/objekt-anzeigen/objekt-anzeigen.component.spec.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/objekt-anzeigen/objekt-anzeigen.component.spec.ts
@@ -2,13 +2,11 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ObjektAnzeigenComponent} from './objekt-anzeigen.component';
 import {RouterTestingModule} from '@angular/router/testing';
-import {BedienkonzeptModule} from '../bedienkonzept/bedienkonzept.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DropdownModule} from 'primeng/dropdown';
-import {TableModule} from 'primeng/table';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {TranslateTestingModule} from 'ngx-translate-testing';
 import {By} from '@angular/platform-browser';
+import {MessageService} from 'primeng/api';
+import {ObjektAnzeigenModule} from './objekt-anzeigen.module';
 
 describe('PersonBearbeitenComponent', () => {
   let component: ObjektAnzeigenComponent;
@@ -20,15 +18,14 @@ describe('PersonBearbeitenComponent', () => {
       declarations: [ObjektAnzeigenComponent],
       imports: [
         RouterTestingModule,
-        BedienkonzeptModule,
+        ObjektAnzeigenModule,
         BrowserAnimationsModule,
-        DropdownModule,
-        TableModule,
-        FormsModule,
-        ReactiveFormsModule,
         TranslateTestingModule.withTranslations('de', {
           'isyAngularWidgetsDemo.labels.optionMale': 'Männlich'
         })
+      ],
+      providers: [
+        MessageService
       ]
     })
       .compileComponents();
--- projects/isy-angular-widgets-demo/src/app/feature/objekt-anzeigen/objekt-anzeigen.module.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/objekt-anzeigen/objekt-anzeigen.module.ts
@@ -22,7 +22,7 @@ import {IncompleteDateModule} from '../../../../../isy-angular-widgets/src/lib/i
 import {InputMaskModule} from 'primeng/inputmask';
 import {CalendarModule} from 'primeng/calendar';
 import {ObjektAnzeigenRoutingModule} from './objekt-anzeigen-routing.module';
-import {InputTextareaModule} from "primeng/inputtextarea";
+import {InputTextareaModule} from 'primeng/inputtextarea';
 
 
 
