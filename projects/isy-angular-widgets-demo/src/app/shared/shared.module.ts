6b5c17c2e9d7c5232743ce8e05fdad4121bc568c
braatt <tim.braatz@msg.group>
2023-07-24 16:29:34 +0200

    refactor: reduced shared module dependencies

--- projects/isy-angular-widgets-demo/src/app/shared/shared.module.ts
+++ projects/isy-angular-widgets-demo/src/app/shared/shared.module.ts
@@ -1,36 +1,12 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {
-  DialogSachverhalteBearbeitenComponent
-} from './components/dialog-sachverhalte-bearbeiten/dialog-sachverhalte-bearbeiten.component';
-import {DialogModule} from 'primeng/dialog';
-import {TableModule} from 'primeng/table';
-import {ButtonModule} from 'primeng/button';
-import {InputTextModule} from 'primeng/inputtext';
-import {RippleModule} from 'primeng/ripple';
-import {FormsModule} from '@angular/forms';
 import {MessageService} from 'primeng/api';
-import {TranslateModule} from '@ngx-translate/core';
+import {PersonenService} from './services/personen.service';
 
 @NgModule({
-  declarations: [
-    DialogSachverhalteBearbeitenComponent
-  ],
-  exports: [
-    DialogSachverhalteBearbeitenComponent
-  ],
-  imports: [
-    CommonModule,
-    DialogModule,
-    TableModule,
-    ButtonModule,
-    InputTextModule,
-    RippleModule,
-    FormsModule,
-    TranslateModule
-  ],
+  imports: [],
   providers: [
-    MessageService
+    MessageService,
+    PersonenService
   ]
 })
 export class SharedModule { }
