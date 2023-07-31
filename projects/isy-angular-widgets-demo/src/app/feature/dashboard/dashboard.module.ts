152cc49908223a063e51be6c7d533df03a66771f
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-07-31 17:47:14 +0200

    feat: ISY-97 Added responsive cols to dashboard view

--- projects/isy-angular-widgets-demo/src/app/feature/dashboard/dashboard.module.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/dashboard/dashboard.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
-
-
+import { QuicklinkComponent } from './components/quicklink/quicklink.component';
+import {CardModule} from 'primeng/card';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, QuicklinkComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CardModule
   ]
 })
 export class DashboardModule { }
