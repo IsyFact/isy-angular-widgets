cc9f5a8c391844318f212fe440d210bd0e68dcfd
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-07-31 13:51:15 +0200

    refactor: ISY-97 Moved plain component into directory

--- projects/isy-angular-widgets-demo/src/app/app-routing.module.ts
+++ projects/isy-angular-widgets-demo/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {DashboardComponent} from './feature/dashboard/dashboard.component';
+import {DashboardComponent} from './feature/dashboard/components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
--- projects/isy-angular-widgets-demo/src/app/feature/dashboard/dashboard.module.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {DashboardComponent} from './dashboard.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 
 
