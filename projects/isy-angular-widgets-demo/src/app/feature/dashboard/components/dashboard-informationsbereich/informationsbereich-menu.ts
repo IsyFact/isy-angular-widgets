3dce867d86c2b0629e81b12ac31d2a5f287ae86b
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-08-04 14:20:22 +0200

    feat: ISY-97 Added sidebar item menus

---
+++ projects/isy-angular-widgets-demo/src/app/feature/dashboard/components/dashboard-informationsbereich/informationsbereich-menu.ts
@@ -0,0 +1,29 @@
+export const informationsbereichMenu = [
+  {
+    label: 'isyAngularWidgetsDemo.menuItems.quickLinks',
+    expanded: true,
+    items: [
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleQuickLink1'
+      },
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleQuickLink2',
+        visible: true
+      }
+    ]
+  },
+  {
+    label: 'isyAngularWidgetsDemo.menuItems.frequentTasks',
+    expanded: true,
+    items: [
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.searchObject',
+        routerLink: 'objekt-suchen'
+      },
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.displayObject',
+        routerLink: 'objekt-anzeigen/42'
+      }
+    ]
+  }
+];
---
+++ projects/isy-angular-widgets-demo/src/app/feature/dashboard/components/dashboard-linksnavigation/linksnavigation-menu.ts
@@ -0,0 +1,15 @@
+export const linksnavigationMenu = [
+  {
+    label: 'isyAngularWidgetsDemo.menuItems.quickLinks',
+    expanded: true,
+    items: [
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleQuickLink1'
+      },
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleQuickLink2',
+        visible: true
+      }
+    ]
+  }
+];
