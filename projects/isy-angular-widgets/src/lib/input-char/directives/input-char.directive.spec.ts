1a66c44286090676df065127e91512b08e95e7fe
mtpham-collab <74294456+mtpham-collab@users.noreply.github.com>
2023-08-08 15:58:25 +0200

    Feature/isy 133 fix error diacritical characters (#64)
    * ISY-133: Fix error with diacritical characters

--- projects/isy-angular-widgets/src/lib/input-char/directives/input-char.directive.spec.ts
+++ projects/isy-angular-widgets/src/lib/input-char/directives/input-char.directive.spec.ts
@@ -64,7 +64,7 @@ describe('InputCharDirective', () => {
 
   it('should set next input position', () => {
     expect(directive.inputMousePosition).toEqual(0);
-    directive.setNextInputPosition();
+    directive.setNextInputPosition(1);
     expect(directive.inputMousePosition).toEqual(1);
   });
 
@@ -83,16 +83,16 @@ describe('InputCharDirective', () => {
   });
 
   it('should check the build of the current input value', () => {
-    const value = 'a';
-    const zeichen = 'A̋';
+    const value = 'e';
+    const zeichen = 'ç̆';
 
     expect(directive.inputMousePosition).toEqual(0);
-    const inputValue = directive.buildInputValue(value, zeichen);
+    let inputValue = directive.buildInputValue(value, zeichen);
+    directive.setNextInputPosition(zeichen.length);
     expect(inputValue).toEqual(`${zeichen}${value}`);
-
-    directive.inputMousePosition = 1;
-    fixture.detectChanges();
-    const newInputValue = directive.buildInputValue(value, zeichen);
-    expect(newInputValue).toEqual(`${value}${zeichen}`);
+    
+    inputValue = directive.buildInputValue(inputValue, value);
+    directive.setNextInputPosition(value.length);
+    expect(inputValue).toEqual(`${zeichen}${value}${value}`);
   });
 });
