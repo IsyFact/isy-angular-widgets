cd88e017d5805c13a3ffa21ec2ca92396c4db36f
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-08-09 12:31:26 +0200

    feat: ISY-97 Added widget menu items

---
+++ projects/isy-angular-widgets-demo/src/app/feature/dashboard/menus.ts
@@ -0,0 +1,63 @@
+export const bereichA = [
+  {
+    label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetFirstAreaTitle',
+    expanded: true,
+    icon: 'pi pi-file',
+    items: [
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetFirstAreaInformation'
+      },
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetFirstAreaEvaluation'
+      }
+    ]
+  }
+];
+
+export const bereichB = [
+  {
+    label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetSecondAreaTitle',
+    expanded: true,
+    icon: 'pi pi-user',
+    items: [
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetSecondAreaDocuments'
+      },
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetSecondAreaRequest'
+      }
+    ]
+  }
+];
+
+export const bereichC = [
+  {
+    label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetThirdAreaTitle',
+    expanded: true,
+    icon: 'pi pi-compass',
+    items: [
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetThirdAreaMessages'
+      },
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetThirdAreaCare'
+      }
+    ]
+  }
+];
+
+export const bereichD = [
+  {
+    label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetFourthAreaTitle',
+    expanded: true,
+    icon: 'pi pi-info-circle',
+    items: [
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetFourthAreaInformation'
+      },
+      {
+        label: 'isyAngularWidgetsDemo.menuItems.exampleWidgetFourthAreaEvaluation'
+      }
+    ]
+  }
+];
