111ea7a33cd075a6ba149564de4c3b26191b8144
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-08-15 13:40:02 +0200

    feat: ISY-138 Removed deprecated test configurations

--- projects/isy-angular-widgets/src/test.ts
+++ projects/isy-angular-widgets/src/test.ts
@@ -8,14 +8,7 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
-
-// First, initialize the Angular testing environment.
+// Initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
