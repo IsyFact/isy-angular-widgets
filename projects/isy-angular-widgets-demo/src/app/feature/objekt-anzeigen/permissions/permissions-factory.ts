329e14b01a9382c98b8594bdc6304884c9c28548
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-08-30 10:24:48 +0200

    feat: ISY-137 Added permissions factory Singleton

---
+++ projects/isy-angular-widgets-demo/src/app/feature/objekt-anzeigen/permissions/permissions-factory.ts
@@ -0,0 +1,32 @@
+import {PermissionsBase} from './permissions-base';
+import {Role} from '../model/auth';
+import {AdminPermissions} from './admin-permissions';
+import {UserPermissions} from './user-permissions';
+import {UnknownPermissions} from './unknown-permissions';
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+export class PermissionsFactory {
+  private static instance: PermissionsBase;
+
+  private constructor() {
+    // private and unused because singleton pattern
+  }
+
+  static getInstance(): PermissionsBase {
+    const role = localStorage.getItem('role');
+    if (role) {
+      switch (role) {
+        case Role.ADMIN:
+          this.instance = new AdminPermissions();
+          break;
+        case Role.USER:
+          this.instance = new UserPermissions();
+          break;
+        default:
+          this.instance = new UnknownPermissions();
+          break;
+      }
+    }
+    return this.instance;
+  }
+}
