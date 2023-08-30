064d3dd0712ef11c11a84b77824b2962046a1a68
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-08-30 10:43:21 +0200

    feat: ISY-137 Added permission data for demo dropdown

---
+++ projects/isy-angular-widgets-demo/src/app/feature/objekt-anzeigen/dropdown-permission-data.ts
@@ -0,0 +1,12 @@
+import {Permission} from './model/permission';
+
+export const personsWithPermissions: Permission[] = [
+  {
+    title: 'Unberechtigt',
+    permitted: false
+  },
+  {
+    title: 'Berechtigt',
+    permitted: true
+  }
+];
