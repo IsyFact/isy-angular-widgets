8f85dc646b2a60ad78c4978b9828eca23bd56741
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-08-30 10:22:54 +0200

    feat: ISY-137 Added roles and permission types

---
+++ projects/isy-angular-widgets-demo/src/app/feature/objekt-anzeigen/model/auth.ts
@@ -0,0 +1,13 @@
+export enum Role {
+  ADMIN = 'admin',
+  USER = 'user',
+  UNKNOWN = 'unknown'
+}
+
+export enum PermissionType {
+  CREATE = 'create',
+  READ = 'read',
+  UPDATE = 'update',
+  DELETE = 'delete',
+  EDIT = 'edit'
+}
