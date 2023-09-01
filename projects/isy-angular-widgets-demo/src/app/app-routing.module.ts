24005be9aa6ac0f1502fa3f3e10765a8378df64f
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-09-01 14:58:16 +0200

    feat: ISY-137 Added auth guards

--- projects/isy-angular-widgets-demo/src/app/app-routing.module.ts
+++ projects/isy-angular-widgets-demo/src/app/app-routing.module.ts
@@ -1,21 +1,29 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-
+import {AuthGuard} from '../../../isy-angular-widgets/src/lib/security/security-guard';
 const routes: Routes = [
   {
-    path: '',
+    path: 'dashboard',
     data: {
       title: 'Dashboard'
     },
-    loadChildren: async() => (await import('./feature/dashboard/dashboard.module')).DashboardModule
+    loadChildren: async() => (await import('./feature/dashboard/dashboard.module')).DashboardModule,
+    canActivate: [AuthGuard]
   },
   {
     path: 'objekt-anzeigen',
-    loadChildren: async() => (await import('./feature/objekt-anzeigen/objekt-anzeigen.module')).ObjektAnzeigenModule
+    loadChildren: async() => (await import('./feature/objekt-anzeigen/objekt-anzeigen.module')).ObjektAnzeigenModule,
+    canActivate: [AuthGuard]
   },
   {
     path: 'objekt-suchen',
-    loadChildren: async() => (await import('./feature/objekt-suchen/objekt-suchen.module')).ObjektSuchenModule
+    loadChildren: async() => (await import('./feature/objekt-suchen/objekt-suchen.module')).ObjektSuchenModule,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
   }
 ];
 
