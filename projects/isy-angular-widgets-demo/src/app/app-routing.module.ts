e97fb01cd54b3ccf9647195bff9e0d781ac31cf9
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-09-05 11:32:16 +0200

    refactor: ISY-137 Added routes as const

--- projects/isy-angular-widgets-demo/src/app/app-routing.module.ts
+++ projects/isy-angular-widgets-demo/src/app/app-routing.module.ts
@@ -1,31 +1,6 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {AuthGuard} from '../../../isy-angular-widgets/src/lib/security/security-guard';
-const routes: Routes = [
-  {
-    path: 'dashboard',
-    data: {
-      title: 'Dashboard'
-    },
-    loadChildren: async() => (await import('./feature/dashboard/dashboard.module')).DashboardModule,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'objekt-anzeigen',
-    loadChildren: async() => (await import('./feature/objekt-anzeigen/objekt-anzeigen.module')).ObjektAnzeigenModule,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'objekt-suchen',
-    loadChildren: async() => (await import('./feature/objekt-suchen/objekt-suchen.module')).ObjektSuchenModule,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  }
-];
+import {RouterModule} from '@angular/router';
+import {routes} from './routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
