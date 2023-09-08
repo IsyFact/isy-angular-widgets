f9833ee083fc2014b173ded1583a78a49ea91407
braatt <tim.braatz@msg.group>
2023-09-08 13:25:29 +0200

    test: corrected input-char-preview tests
    Signed-off-by: braatt <tim.braatz@msg.group>

--- projects/isy-angular-widgets/src/lib/input-char/components/input-char-preview/input-char-preview.component.spec.ts
+++ projects/isy-angular-widgets/src/lib/input-char/components/input-char-preview/input-char-preview.component.spec.ts
@@ -38,48 +38,27 @@ describe('InputCharViewComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should check the incoming zeichenobjekt input', () => {
-    expect(component.zeichenObjekt.zeichen).toEqual(zeichenObjekt.zeichen);
-    expect(component.zeichenObjekt.grundzeichen).toEqual(zeichenObjekt.grundzeichen);
-    expect(component.zeichenObjekt.schriftzeichengruppe).toEqual(zeichenObjekt.schriftzeichengruppe);
-    expect(component.zeichenObjekt.name).toEqual(zeichenObjekt.name);
-    expect(component.zeichenObjekt.codepoint).toEqual(zeichenObjekt.codepoint);
-  });
-
-  it('should check emitted zeichenobjekt on selection', () => {
-    const zeichenObjektChangedSpy = spyOn(component.zeichenObjektChange, 'emit');
-    component.selectZeichen(zeichenObjekt);
-    expect(zeichenObjektChangedSpy).toHaveBeenCalledWith(zeichenObjekt);
-  });
-
-  it('should check the select button label', () => {
+  it('should have a button with label "Einfügen"', () => {
     const insertButton = fixture.debugElement.query(By.css('.insert-button')).nativeElement as HTMLElement;
     expect(insertButton.innerText).toContain('Einfügen');
   });
 
-  it('should check the select button click functionality', () => {
-    const zeichenObjektChangedSpy = spyOn(component.zeichenObjektChange, 'emit');
-    const insertButton = fixture.debugElement.query(By.css('.insert-button')).nativeElement as HTMLElement;
-    insertButton.click();
-    expect(zeichenObjektChangedSpy).toHaveBeenCalledWith(zeichenObjekt);
-  });
-
-  it('should check the character serif information content', () => {
-    const serifCharacterPreview = fixture.debugElement.query(By.css('.serif')).nativeElement as HTMLElement;
+  it('should show the selected character in the sans field', () => {
+    const serifCharacterPreview = fixture.debugElement.query(By.css('#serif-letter')).nativeElement as HTMLElement;
     expect(serifCharacterPreview.innerText).toContain(zeichenObjekt.zeichen);
   });
 
-  it('should check the character sans-serif information content', () => {
-    const sansSerifCharacterPreview = fixture.debugElement.query(By.css('.sans-serif')).nativeElement as HTMLElement;
+  it('should show the selected character sans-serif field', () => {
+    const sansSerifCharacterPreview = fixture.debugElement.query(By.css('#sans-letter')).nativeElement as HTMLElement;
     expect(sansSerifCharacterPreview.innerText).toContain(zeichenObjekt.zeichen);
   });
 
-  it('should check the character description name', () => {
+  it('should show selected the character description', () => {
     const characterDescriptionName = fixture.debugElement.query(By.css('#description')).nativeElement as HTMLElement;
     expect(characterDescriptionName.innerText).toContain(zeichenObjekt.name);
   });
 
-  it('should check the character codepoint', () => {
+  it('should show selected the character codepoint', () => {
     const characterDescriptionName = fixture.debugElement.query(By.css('#codepoint')).nativeElement as HTMLElement;
     expect(characterDescriptionName.innerText).toContain(zeichenObjekt.codepoint);
   });
