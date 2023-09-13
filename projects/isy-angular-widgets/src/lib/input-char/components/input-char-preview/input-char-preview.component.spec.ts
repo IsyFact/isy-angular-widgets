756f96fc94986112dade0777b0178f29b006f242
braatt <tim.braatz@msg.group>
2023-09-13 13:48:48 +0200

    test: added tests for all characters
    Signed-off-by: braatt <tim.braatz@msg.group>

--- projects/isy-angular-widgets/src/lib/input-char/components/input-char-preview/input-char-preview.component.spec.ts
+++ projects/isy-angular-widgets/src/lib/input-char/components/input-char-preview/input-char-preview.component.spec.ts
@@ -1,36 +1,25 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {InputCharPreviewComponent} from './input-char-preview.component';
-import {Schriftzeichengruppe, Zeichenobjekt} from '../../model/model';
-import {ButtonModule} from 'primeng/button';
+import {Zeichenobjekt} from '../../model/model';
 import {By} from '@angular/platform-browser';
+import sonderzeichenliste from '../../sonderzeichenliste.json';
 
 describe('InputCharViewComponent', () => {
   let component: InputCharPreviewComponent;
   let fixture: ComponentFixture<InputCharPreviewComponent>;
-
-  const zeichenObjekt: Zeichenobjekt =   {
-    zeichen: 'A',
-    grundzeichen: 'A',
-    schriftzeichengruppe: Schriftzeichengruppe.LATEIN,
-    name: 'LATIN CAPITAL LETTER A',
-    codepoint: 'U+0041'
-  };
+  const sonderzeichenListe = sonderzeichenliste as Zeichenobjekt[];
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
       declarations: [
         InputCharPreviewComponent
-      ],
-      imports: [
-        ButtonModule
       ]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(InputCharPreviewComponent);
     component = fixture.componentInstance;
-    component.zeichenObjekt = zeichenObjekt;
     fixture.detectChanges();
   });
 
@@ -38,23 +27,38 @@ describe('InputCharViewComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show the selected character in the sans field', () => {
-    const serifCharacterPreview = fixture.debugElement.query(By.css('#serif-letter')).nativeElement as HTMLElement;
-    expect(serifCharacterPreview.innerText).toContain(zeichenObjekt.zeichen);
-  });
+  sonderzeichenListe.forEach((zeichenObjekt: Zeichenobjekt) => {
+    it(`should show the selected character ${zeichenObjekt.zeichen} in the sans field`, () => {
+      component.zeichenObjekt = zeichenObjekt;
+      fixture.detectChanges();
+      console.log(zeichenObjekt);
 
-  it('should show the selected character sans-serif field', () => {
-    const sansSerifCharacterPreview = fixture.debugElement.query(By.css('#sans-letter')).nativeElement as HTMLElement;
-    expect(sansSerifCharacterPreview.innerText).toContain(zeichenObjekt.zeichen);
-  });
+      const serifCharacterPreview = fixture.debugElement.query(By.css('#serif-letter')).nativeElement as HTMLElement;
+      expect(serifCharacterPreview.innerText).toContain(zeichenObjekt.zeichen);
+    });
 
-  it('should show selected the character description', () => {
-    const characterDescriptionName = fixture.debugElement.query(By.css('#description')).nativeElement as HTMLElement;
-    expect(characterDescriptionName.innerText).toContain(zeichenObjekt.name);
-  });
+    it(`should show the selected character ${zeichenObjekt.zeichen} sans-serif field`, () => {
+      component.zeichenObjekt = zeichenObjekt;
+      fixture.detectChanges();
 
-  it('should show selected the character codepoint', () => {
-    const characterDescriptionName = fixture.debugElement.query(By.css('#codepoint')).nativeElement as HTMLElement;
-    expect(characterDescriptionName.innerText).toContain(zeichenObjekt.codepoint);
+      const sansSerifCharacterPreview = fixture.debugElement.query(By.css('#sans-letter')).nativeElement as HTMLElement;
+      expect(sansSerifCharacterPreview.innerText).toContain(zeichenObjekt.zeichen);
+    });
+
+    it(`should show selected the character ${zeichenObjekt.zeichen} description`, () => {
+      component.zeichenObjekt = zeichenObjekt;
+      fixture.detectChanges();
+
+      const characterDescriptionName = fixture.debugElement.query(By.css('#description')).nativeElement as HTMLElement;
+      expect(characterDescriptionName.innerText).toContain(zeichenObjekt.name);
+    });
+
+    it(`should show selected the character ${zeichenObjekt.zeichen} codepoint`, () => {
+      component.zeichenObjekt = zeichenObjekt;
+      fixture.detectChanges();
+
+      const characterDescriptionName = fixture.debugElement.query(By.css('#codepoint')).nativeElement as HTMLElement;
+      expect(characterDescriptionName.innerText).toContain(zeichenObjekt.codepoint);
+    });
   });
 });
