c838e89f2d18582c66bc498bc725932804fd8925
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-10-23 16:51:35 +0200

    feat: ISY-360 Added route for error page

--- projects/isy-angular-widgets-demo/src/app/shared/shared.module.ts
+++ projects/isy-angular-widgets-demo/src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import {NgModule} from '@angular/core';
 import {PersonenService} from './services/personen.service';
+import {PageNotFoundComponent} from './errors/page-not-found/page-not-found.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  imports: [],
-  providers: [PersonenService]
+  imports: [RouterModule],
+  providers: [PersonenService],
+  declarations: [PageNotFoundComponent]
 })
 export class SharedModule {}
