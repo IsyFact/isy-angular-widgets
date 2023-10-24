2c1ee3e179583be3fa9f2532cd7ff0ae77ae203c
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-10-24 19:21:46 +0200

    feat: ISY-144 Updated dependencies

--- projects/isy-angular-widgets/schematics/ng-add/index.ts
+++ projects/isy-angular-widgets/schematics/ng-add/index.ts
@@ -16,7 +16,7 @@ export function ngAdd(): Rule {
     addPackageToPackageJson(tree, '@angular/common', '^16.2.9');
     addPackageToPackageJson(tree, '@angular/core', '^16.2.9');
     addPackageToPackageJson(tree, 'primeicons', '^6.0.1');
-    addPackageToPackageJson(tree, 'primeng', '^16.5.0');
+    addPackageToPackageJson(tree, 'primeng', '^16.5.1');
     addPackageToPackageJson(tree, 'primeflex', '^3.3.1');
     addPackageToPackageJson(tree, 'moment', '^2.29.4');
 
