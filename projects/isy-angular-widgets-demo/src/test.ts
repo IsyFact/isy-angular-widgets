4d9a1aebdaae36002479ac2791cdf1d7089640b7
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-11-07 14:35:09 +0100

    feat: ISY-376 Added potential performance fix

--- projects/isy-angular-widgets-demo/src/test.ts
+++ projects/isy-angular-widgets-demo/src/test.ts
@@ -3,8 +3,11 @@
 import 'zone.js/testing';
 import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
+import {ɵSharedStylesHost} from '@angular/platform-browser';
 
 // Initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   teardown: {destroyAfterEach: true}
 });
+
+afterEach(() => getTestBed().inject(ɵSharedStylesHost).ngOnDestroy());
