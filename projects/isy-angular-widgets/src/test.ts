e128209039164a69794d48da6176a8403233e61b
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-11-13 14:27:32 +0100

    fix: ISY-376 Fixed spectator performance issue

--- projects/isy-angular-widgets/src/test.ts
+++ projects/isy-angular-widgets/src/test.ts
@@ -4,8 +4,11 @@ import 'zone.js';
 import 'zone.js/testing';
 import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
+import {ɵSharedStylesHost} from '@angular/platform-browser';
 
 // Initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   teardown: {destroyAfterEach: true}
 });
+
+afterEach(() => getTestBed().inject(ɵSharedStylesHost).ngOnDestroy());
