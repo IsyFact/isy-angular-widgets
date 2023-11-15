a1e3ef57953e0d5220ea371b7b722424b7545529
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-11-15 11:50:07 +0100

    test: ISY-376 Reduced test length and mocked method result

--- projects/isy-angular-widgets/src/lib/security/security-guard.spec.ts
+++ projects/isy-angular-widgets/src/lib/security/security-guard.spec.ts
@@ -1,36 +1,7 @@
 import {SecurityService} from './security-service';
-import {ActivatedRoute, ActivatedRouteSnapshot, UrlSegment} from '@angular/router';
+import {ActivatedRouteSnapshot, UrlSegment} from '@angular/router';
 import {AuthGuard} from './security-guard';
-import {of} from 'rxjs';
-import {PermissionMaps} from './permission-maps';
-import {UserInfo} from '../api/userinfo';
-import SpyObj = jasmine.SpyObj;
-import {createServiceFactory, SpectatorService} from '@ngneat/spectator';
-import {TestBed} from '@angular/core/testing';
-
-let fakeCounterService: SpyObj<AuthGuard>;
-
-/**
- * Setting up the fake counter service
- * @param canActivate The canActivate value
- */
-function setupFakeCounterService(canActivate: boolean): void {
-  fakeCounterService = jasmine.createSpyObj<AuthGuard>('AuthGuard', {
-    canActivate: of(canActivate)
-  });
-}
-
-/**
- *  Returns needed logged-in user info data
- *  @returns an object with logged-in user info
- */
-function getUserInfo(): UserInfo {
-  return {
-    userId: '1',
-    roles: ['admin', 'user'],
-    displayName: 'Nutzer'
-  };
-}
+import {createServiceFactory, createSpyObject, SpectatorService} from '@ngneat/spectator';
 
 /**
  * builds an activated route snapshot
@@ -42,36 +13,11 @@ function buildSnapshot(): ActivatedRouteSnapshot {
   return snapshot;
 }
 
-let securityService: SecurityService;
 const snapshot = buildSnapshot();
-const activatedRouteProvider = {
-  provide: ActivatedRoute,
-  useValue: {
-    data: {
-      title: 'Dashboard'
-    },
-    outlet: 'primary',
-    routerConfig: {
-      data: {
-        title: 'Dashboard'
-      },
-      path: 'dashboard'
-    },
-    url: [
-      {
-        parameters: {},
-        path: 'dashboard'
-      }
-    ]
-  }
-};
 
-describe('Integration Test: SecurityGuard - without setting up roles and permissions', function () {
+describe('Integration Test: SecurityGuard - without setting up roles and permissions', ()=> {
   let spectator: SpectatorService<AuthGuard>;
-  const createdService = createServiceFactory({
-    service: AuthGuard,
-    providers: [{provide: AuthGuard, useValue: {}}]
-  });
+  const createdService = createServiceFactory(AuthGuard);
 
   beforeEach(() => spectator = createdService());
 
@@ -87,43 +33,24 @@ describe('Integration Test: SecurityGuard - without setting up roles and permiss
   });
 });
 
-describe('Integration Test: SecurityGuard - with setting up roles and permissions', function () {
-  let guard: AuthGuard;
-  const permissionsData: PermissionMaps = {
-    elements: {
-      personenSuche: ['admin', 'user'],
-      secretFieldsInputSwitch: ['admin']
-    },
-    routes: {
-      personen: ['admin', 'user'],
-      dashboard: ['admin', 'user']
-    }
-  };
+describe('Integration Test: SecurityGuard - with setting up roles and permissions', ()=> {
+  const securityServiceSpy = createSpyObject(SecurityService);
+  securityServiceSpy.checkRoutePermission.andReturn(true);
 
-  beforeEach(() => {
-    setupFakeCounterService(true);
-    TestBed.configureTestingModule({
-      declarations: [],
-      imports: [],
-      providers: [{provide: AuthGuard, useValue: fakeCounterService}, SecurityService, activatedRouteProvider]
-    });
-
-    guard = TestBed.inject(AuthGuard);
-    securityService = TestBed.inject(SecurityService);
-
-    const userInfoData = getUserInfo();
-    securityService.setRoles(userInfoData);
-    securityService.setPermissions(permissionsData);
+  let spectator: SpectatorService<AuthGuard>;
+  const createdService = createServiceFactory({
+    service: AuthGuard,
+    providers: [{provide: SecurityService, useValue: securityServiceSpy}]
   });
 
+  beforeEach(() => spectator = createdService());
+
   it('should create', () => {
-    expect(guard).toBeTruthy();
+    expect(spectator.service).toBeTruthy();
   });
 
   it('should activate - with roles set up', () => {
-    const canActivateObservable = guard.canActivate(snapshot);
-    void canActivateObservable.forEach((canActivate) => {
-      expect(canActivate).toBeTrue();
-    });
+    const canActivateObservable = spectator.service.canActivate(snapshot);
+    expect(canActivateObservable).toBeTrue();
   });
 });
