6d0a0014b931aad575a74c5b55d6e18bc0339b9c
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-11-17 12:47:36 +0100

    refactor: ISY-624 Fixed SonarLint regex warnings

--- projects/isy-angular-widgets/src/lib/incomplete-date/data/regex.ts
+++ projects/isy-angular-widgets/src/lib/incomplete-date/data/regex.ts
@@ -1,3 +1,3 @@
-export const INCOMPLETE_DATE_REGEX: RegExp = /^(0{2}\.x{2}\.\d{4})|(x{2}\.0{2}\.\d{4})$/;
+export const INCOMPLETE_DATE_REGEX: RegExp = /^(0{2}|x{2})\.(x{2}|0{2})\.\d{4}$/;
 
-export const UNSPECIFIED_DATE_REGEX: RegExp = /^(0{2}\.\d{2}\.\d{4})|(\d{2}\.0{2}\.\d{4})$/;
+export const UNSPECIFIED_DATE_REGEX: RegExp = /^(00|\d{2})\.\d{2}\.\d{4}$/;
