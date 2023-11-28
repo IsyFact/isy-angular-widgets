0a2d51c24a49ae0e2e60846402a25189ff3225e6
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-11-28 14:07:23 +0100

    fix: ISY-631 Added new form helper method and deleted unused method

--- projects/isy-angular-widgets-demo/src/app/shared/validation/form-helper.ts
+++ projects/isy-angular-widgets-demo/src/app/shared/validation/form-helper.ts
@@ -1,4 +1,12 @@
-import {FormGroup} from '@angular/forms';
+import {AbstractControl, FormGroup} from '@angular/forms';
+
+/**
+ * Marks a specific form control as dirty
+ * @param control The affected form control
+ */
+export function markFormControlAsDirty(control: AbstractControl<unknown>): void {
+  control.markAsDirty();
+}
 
 /**
  * Marks all fields of a form as dirty.
@@ -11,22 +19,11 @@ export function markFormAsDirty(form: FormGroup): void {
 }
 
 /**
- * Marks an array of multiple forms as dirty.
- * @param formArray The form array containing all forms
- */
-export function markFormArrayAsDirty(formArray: FormGroup[]): void {
-  formArray.forEach((form) => {
-    markFormAsDirty(form);
-  });
-}
-
-/**
  * Resets the fields of a form and marks it as dirty.
  * @param form The shape to reset
  */
 export function resetForm(form: FormGroup): void {
   Object.keys(form.controls).forEach((key) => {
     form.controls[key].reset();
-    form.controls[key].markAsDirty();
   });
 }
