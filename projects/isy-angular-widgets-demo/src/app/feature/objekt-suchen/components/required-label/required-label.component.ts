f1d58fdd8d5dab75dec20f67e759ba5f26bafd9b
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-11-28 15:38:36 +0100

    feat: ISY-631 Added component for required labels

---
+++ projects/isy-angular-widgets-demo/src/app/feature/objekt-suchen/components/required-label/required-label.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-required-label',
+  templateUrl: './required-label.component.html',
+  styleUrls: ['./required-label.component.scss']
+})
+export class RequiredLabelComponent {
+
+}
