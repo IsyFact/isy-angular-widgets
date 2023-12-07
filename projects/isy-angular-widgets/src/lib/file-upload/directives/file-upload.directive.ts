39fd28c12a82caf038e762355f3474b1e237b8e1
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-12-07 11:10:09 +0100

    fix: ISY-575 Reduced code

--- projects/isy-angular-widgets/src/lib/file-upload/directives/file-upload.directive.ts
+++ projects/isy-angular-widgets/src/lib/file-upload/directives/file-upload.directive.ts
@@ -1,6 +1,5 @@
 import {Directive, forwardRef} from '@angular/core';
-import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
-import {FileUpload} from 'primeng/fileupload';
+import {NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Directive({
   // eslint-disable-next-line @angular-eslint/directive-selector
@@ -13,25 +12,6 @@ import {FileUpload} from 'primeng/fileupload';
     }
   ]
 })
-export class FileUploadDirective implements ControlValueAccessor {
-  constructor(private fileUpload: FileUpload) {}
+export class FileUploadDirective {
 
-  writeValue(value: unknown): void {
-    console.log(value);
-    // update the model and changes logic goes here
-  }
-
-  registerOnChange(fn: unknown): void {
-    // notify the outside world about changes (when the user interacts with the input)
-    this.fileUpload.onUpload.subscribe(fn);
-  }
-
-  registerOnTouched(fn: unknown): void {
-    // here goes the touch logic
-    this.fileUpload.onClear.subscribe(fn);
-  }
-
-  setDisabledState(isDisabled: boolean): void {
-    this.fileUpload.disabled = isDisabled;
-  }
 }
