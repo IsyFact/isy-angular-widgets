8392c7b668f962755fc7012b27f883a170db5a95
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-12-14 16:00:53 +0100

    revert: ISY-722 Reverted deleted regex

--- projects/isy-angular-widgets/src/lib/validation/data/date-formats.ts
+++ projects/isy-angular-widgets/src/lib/validation/data/date-formats.ts
@@ -1,3 +1,5 @@
+export const ISO_8601_REGEX: RegExp = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d+)?)(Z|[+-]\d{2}:\d{2})$/;
+
 export const INPUT_MASK_REGEX: RegExp = /^([x0-9]{2}\.[x0-9]{2}\.[x0-9]{4})$/;
 
 export const INPUT_UNSPECIFIED_REGEX = new RegExp(
