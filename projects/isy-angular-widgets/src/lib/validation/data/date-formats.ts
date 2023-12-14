0126f0f756e9c54df53d6d36e248e6075e71a912
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2023-12-14 15:57:30 +0100

    fix: ISY-722 Fixed SonarQube Error & removed unused Regex

--- projects/isy-angular-widgets/src/lib/validation/data/date-formats.ts
+++ projects/isy-angular-widgets/src/lib/validation/data/date-formats.ts
@@ -1,9 +1,14 @@
-export const ISO_8601_REGEX: RegExp = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d+)?)(Z|[+-]\d{2}:\d{2})$/;
-
 export const INPUT_MASK_REGEX: RegExp = /^([x0-9]{2}\.[x0-9]{2}\.[x0-9]{4})$/;
 
-export const INPUT_UNSPECIFIED_REGEX: RegExp =
-  /^(0{2}\.([0-1][1-2]|1[0-2])\.\d{4})$|^(0{2}\.0{2}\.\d{4})$|^(0{2}\.0{2}\.0{4})$|^(x{2}\.([0-1][1-2]|1[0-2])\.\d{4})$|^(x{2}\.x{2}\.\d{4})$|^(x{2}\.x{2}\.x{4})$/;
+export const INPUT_UNSPECIFIED_REGEX = new RegExp(
+  [
+    '/^(0{2}\\.([0-1][1-2]|1[0-2])\\.\\d{4})$|^(0{2}\\.0{2}\\.\\d{4})$|^(0{2}\\.0{2}\\.0{4})$|',
+    '^(x{2}\\.([0-1][1-2]|1[0-2])\\.\\d{4})$|^(x{2}\\.x{2}\\.\\d{4})$|^(x{2}\\.x{2}\\.x{4})$/'
+  ]
+    .join(',')
+    .replace(/,/g, '')
+    .replace(/\//g, '')
+);
 
 // date-fns employs single quote symbols for character escaping, such as 'T'.
 // This can create conflicts when using ESLint alongside Prettier for code formatting. Consequently, singlequotes are deactivated in the following line.
