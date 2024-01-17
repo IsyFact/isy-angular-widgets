46db1511a46024b6d53e71bc7ae622a14959f2f7
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2024-01-17 16:06:37 +0100

    test: ISY-722 Added test

--- projects/isy-angular-widgets/src/lib/input-char/components/input-char-dialog-button-selection-side/input-char-dialog-button-selection-side.component.spec.ts
+++ projects/isy-angular-widgets/src/lib/input-char/components/input-char-dialog-button-selection-side/input-char-dialog-button-selection-side.component.spec.ts
@@ -1,19 +1,41 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-
 import {InputCharDialogButtonSelectionSideComponent} from './input-char-dialog-button-selection-side.component';
+import {createComponentFactory, Spectator} from '@ngneat/spectator';
+import {MockModule} from 'ng-mocks';
+import {AccordionModule} from 'primeng/accordion';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {FormsModule} from '@angular/forms';
+import sonderzeichenliste from '../../sonderzeichenliste.json';
+import {Zeichenobjekt} from '../../model/model';
 
-describe('InputCharDialogButtonSelectionSideComponent', () => {
-  let component: InputCharDialogButtonSelectionSideComponent;
-  let fixture: ComponentFixture<InputCharDialogButtonSelectionSideComponent>;
+let spectator: Spectator<InputCharDialogButtonSelectionSideComponent>;
+let component: InputCharDialogButtonSelectionSideComponent;
+const sonderzeichenListe = sonderzeichenliste as Zeichenobjekt[];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [InputCharDialogButtonSelectionSideComponent]
-    }).compileComponents();
+describe('Unit Tests: InputCharDialogButtonSelectionSideComponent', () => {
+  const createComponent = createComponentFactory({
+    component: InputCharDialogButtonSelectionSideComponent,
+    imports: [MockModule(AccordionModule), MockModule(SelectButtonModule), MockModule(FormsModule)]
+  });
 
-    fixture = TestBed.createComponent(InputCharDialogButtonSelectionSideComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    spectator = createComponent();
+    component = spectator.component;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+describe('Integration Tests: InputCharDialogButtonSelectionSideComponent', () => {
+  const createComponent = createComponentFactory({
+    component: InputCharDialogButtonSelectionSideComponent,
+    imports: [SelectButtonModule, AccordionModule, FormsModule]
+  });
+
+  beforeEach(() => {
+    spectator = createComponent();
+    component = spectator.component;
   });
 
   it('should create', () => {
