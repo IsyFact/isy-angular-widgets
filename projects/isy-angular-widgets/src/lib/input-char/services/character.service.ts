d305b1a4e256e424db19526961d0acfde581bcfc
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2024-01-19 19:01:53 +0100

    refactor: ISY-722 Moved functionality to the correct service

--- projects/isy-angular-widgets/src/lib/input-char/services/character.service.ts
+++ projects/isy-angular-widgets/src/lib/input-char/services/character.service.ts
@@ -11,6 +11,26 @@ export class CharacterService {
   getCharacters(): Zeichenobjekt[] {
     return sonderzeichenliste as Zeichenobjekt[];
   }
+  getGrundzeichen(list: Zeichenobjekt[]): string[] {
+    const res = [...new Set(list.map((item) => (item.grundzeichen === '' ? '*' : item.grundzeichen)))];
+
+    // Put * to the first position if present
+    const specialPos = res.indexOf('*');
+    if (specialPos > 0) {
+      res.splice(specialPos, 1);
+      res.unshift('*');
+    }
+    return res;
+  }
+  getSchriftzeichenGruppen(list: Zeichenobjekt[]): Schriftzeichengruppe[] {
+    const res: Schriftzeichengruppe[] = [];
+    for (const char of list) {
+      if (!res.includes(char.schriftzeichengruppe)) {
+        res.push(char.schriftzeichengruppe);
+      }
+    }
+    return res;
+  }
   getCharactersByDataType(datentyp: Datentyp): Zeichenobjekt[] {
     const allowedGroups = this.getGroupsByDataType(datentyp);
     return this.getCharacters().filter((z) => allowedGroups.includes(z.schriftzeichengruppe));
@@ -50,5 +70,11 @@ export class CharacterService {
           Schriftzeichengruppe.KYRILLISCH
         ];
     }
+  }
+  filterByGrundzeichen(list: Zeichenobjekt[], grundzeichen?: string): Zeichenobjekt[] {
+    return list.filter((z) => (z.grundzeichen === '' ? '*' : z.grundzeichen) === grundzeichen);
+  }
+  filterBySchriftzeichenGruppe(list: Zeichenobjekt[], schriftzeichenGruppe?: Schriftzeichengruppe): Zeichenobjekt[] {
+    return list.filter((z) => z.schriftzeichengruppe === schriftzeichenGruppe);
   }
 }
