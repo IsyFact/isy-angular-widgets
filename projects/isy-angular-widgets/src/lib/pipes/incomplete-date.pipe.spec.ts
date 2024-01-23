1d1e4d894855a5cf2133edc7b4e46ed347466cf1
Thao Pham <info@mtpham.de>
2024-01-23 22:36:59 +0100

    ISY-682: Incomplete date pipe added

---
+++ projects/isy-angular-widgets/src/lib/pipes/incomplete-date.pipe.spec.ts
@@ -0,0 +1,73 @@
+import {createPipeFactory, SpectatorPipe} from '@ngneat/spectator';
+import {IncompleteDatePipe} from './incomplete-date.pipe';
+
+fdescribe('Unit Tests: IncompleteDatePipe', () => {
+  let spectator: SpectatorPipe<IncompleteDatePipe>;
+  const createPipe = createPipeFactory(IncompleteDatePipe);
+
+  it('should format "2024-01-01" to "01.01.2024"', () => {
+    spectator = createPipe('{{ "2024-01-01" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('01.01.2024');
+  });
+
+  it('should format incomplete dates in uppercase like "XXXX-XX-XX"', () => {
+    spectator = createPipe('{{ "XXXX-XX-XX" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('xx.xx.xxxx');
+  });
+
+  it('should format incomplete dates like "xxxx-xx-xx"', () => {
+    spectator = createPipe('{{ "xxxx-xx-xx" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('xx.xx.xxxx');
+  });
+
+  it('should format incomplete dates like "2024-xx-xx"', () => {
+    spectator = createPipe('{{ "2024-xx-xx" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('xx.xx.2024');
+  });
+
+  it('should format incomplete dates like "2024-01-xx"', () => {
+    spectator = createPipe('{{ "2024-01-xx" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('xx.01.2024');
+  });
+
+  it('should format incomplete dates like "0000-00-00"', () => {
+    spectator = createPipe('{{ "0000-00-00" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('00.00.0000');
+  });
+
+  it('should format incomplete dates like "2024-00-00"', () => {
+    spectator = createPipe('{{ "2024-00-00" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('00.00.2024');
+  });
+
+  it('should format incomplete dates like "2024-01-00"', () => {
+    spectator = createPipe('{{ "2024-01-00" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('00.01.2024');
+  });
+
+  it('should throw an error for invalid date formats', () => {
+    expect(() => {
+      spectator = createPipe('{{ "invalid-date" | incompleteDatePipe }}');
+    }).toThrow();
+  });
+
+  it('should return an empty string for empty input', () => {
+    spectator = createPipe('{{ "" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('');
+  });
+
+  it('should return an empty string for null input', () => {
+    spectator = createPipe('{{ null | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('');
+  });
+
+  it('should return an empty string for undefined input', () => {
+    spectator = createPipe('{{ undefined | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('');
+  });
+
+  it('should return the same date string in lowercase if in German format', () => {
+    spectator = createPipe('{{ "xx.xx.xxxx" | incompleteDatePipe }}');
+    expect(spectator.element).toHaveText('xx.xx.xxxx');
+  });
+});
---
+++ projects/isy-angular-widgets/src/lib/pipes/incomplete-date.pipe.ts
@@ -0,0 +1,48 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {INCOMPLETE_DATE_GERMAN_FORMAT_REGEX, INCOMPLETE_DATE_REGEX} from './data/regex';
+import {DATE_FORMAT_ERROR} from './data/errors';
+
+/**
+ * Converts an unspecified date string from the format YYYY-MM-DD to the German date format DD.MM.YYYY.
+ * Example: '2024-00-00' becomes '00.00.2024' or 2024-xx-xx becomes xx.xx.2024.
+ * If 'xx' or '00' are not present, e.g. '2024-01-01' becomes '01.01.2024'.
+ * An error is thrown if the input does not match YYYY-MM-DD or DD.MM.YYYY.
+ */
+@Pipe({
+  name: 'incompleteDatePipe',
+  standalone: true
+})
+export class IncompleteDatePipe implements PipeTransform {
+  /**
+   * Converts an unspecified date string to the German format DD.MM.YYYY.
+   * Throws an error for invalid date formats.
+   * @param value Date string in YYYY-MM-DD or DD.MM.YYYY format.
+   * @returns Date string in DD.MM.YYYY format.
+   */
+  transform(value: string | null | undefined): string {
+    if (!value || value === '') return '';
+
+    const valueInLowerCase = value.toLowerCase();
+
+    // Check if the value matches the German date format and return as is if it does
+    if (INCOMPLETE_DATE_GERMAN_FORMAT_REGEX.test(valueInLowerCase)) return valueInLowerCase;
+
+    // Check if the value matches a specified date format, throw an error if it doesn't
+    if (!INCOMPLETE_DATE_REGEX.test(valueInLowerCase)) throw new Error(DATE_FORMAT_ERROR);
+
+    const [year, month, day] = valueInLowerCase.split('-');
+
+    return this.formatDate(year, month, day);
+  }
+
+  /**
+   * Private helper method to format the date components into a day.month.year format
+   * @param year The year string
+   * @param month The month string
+   * @param day The day string
+   * @returns The date string in German date format
+   */
+  private formatDate(year: string, month: string, day: string): string {
+    return [day, month, year].join('.');
+  }
+}
