bb18bce49c626c496d9dcf43553c14c3636910d2
msg-ptavoul <panagiotis.tavoulareas@msg.group>
2024-02-01 11:59:06 +0100

    tests: ISY-722 Added new tests for the left side buttons

--- projects/isy-angular-widgets/src/lib/input-char/components/input-char-dialog-button-selection-side/input-char-dialog-button-selection-side.component.spec.ts
+++ projects/isy-angular-widgets/src/lib/input-char/components/input-char-dialog-button-selection-side/input-char-dialog-button-selection-side.component.spec.ts
@@ -5,11 +5,29 @@ import {AccordionModule} from 'primeng/accordion';
 import {SelectButtonModule} from 'primeng/selectbutton';
 import {FormsModule} from '@angular/forms';
 import sonderzeichenliste from '../../sonderzeichenliste.json';
-import {Zeichenobjekt} from '../../model/model';
+import {InputCharData, Zeichenobjekt} from '../../model/model';
 
 let spectator: Spectator<InputCharDialogButtonSelectionSideComponent>;
 let component: InputCharDialogButtonSelectionSideComponent;
-const sonderzeichenListe = sonderzeichenliste as Zeichenobjekt[];
+
+const charList = sonderzeichenliste as Zeichenobjekt[];
+const bases = [...new Set(charList.map((item) => (item.grundzeichen === '' ? '*' : item.grundzeichen)))];
+const groups = [...new Set(charList.map((item) => item.schriftzeichengruppe))];
+
+const headerStr = 'Alle';
+const inputData: InputCharData[] = [{Basis: bases}, {Gruppen: groups}];
+
+/**
+ * Adds a click event listener to the given button
+ * @param button The button who needs an event
+ * @param event Event who must be
+ * @param param Zeichen method parameter
+ */
+function addClickEventListenerForSelection(button: HTMLButtonElement, event: string, param: string): void {
+  button.addEventListener(event, function () {
+    spectator.component.onSelection(param);
+  });
+}
 
 describe('Unit Tests: InputCharDialogButtonSelectionSideComponent', () => {
   const createComponent = createComponentFactory({
@@ -18,13 +36,35 @@ describe('Unit Tests: InputCharDialogButtonSelectionSideComponent', () => {
   });
 
   beforeEach(() => {
-    // ToDo: Init groups
     spectator = createComponent();
     component = spectator.component;
+    component.allButtonOptions = headerStr;
+    component.dataToDisplay = inputData;
+    spectator.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('all select button should have the correct option value', () => {
+    const allSelectButton = spectator.query('#all-select-button') as HTMLButtonElement;
+    const allSelectButtonAttributes = allSelectButton.attributes;
+
+    const allSelectButtonInnerText = allSelectButtonAttributes.getNamedItem('ng-reflect-options')?.textContent;
+    expect(allSelectButtonInnerText).toEqual(headerStr);
+  });
+
+  it('should firing event on all button click', () => {
+    const spy = spyOn(component, 'onSelection');
+    const allSelectButton = spectator.query('#all-select-button') as HTMLButtonElement;
+    const eventType = 'onChange';
+
+    addClickEventListenerForSelection(allSelectButton, eventType, '');
+    allSelectButton.dispatchEvent(new Event(eventType));
+    spectator.detectChanges();
+
+    expect(spy).toHaveBeenCalledWith('');
   });
 });
 
@@ -35,9 +75,11 @@ describe('Integration Tests: InputCharDialogButtonSelectionSideComponent', () =>
   });
 
   beforeEach(() => {
-    // ToDo: Init groups
     spectator = createComponent();
     component = spectator.component;
+    component.allButtonOptions = headerStr;
+    component.dataToDisplay = inputData;
+    spectator.detectChanges();
   });
 
   it('should create', () => {
