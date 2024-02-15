fc4e87449730d86ede5e75fc642eb8e2276d85e6
msg-tbrtz <81226864+msg-tbrtz@users.noreply.github.com>
2024-02-15 12:00:50 +0100

    fix: ISY-905 Hauptfesnter side bars now collapsing correctly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,16 +1,18 @@
 # 17.2.0 - 
 ## Fixes
-- ISY-775: Die beiden Readme-Dateien wurden für Standalone-Prokjekte aktualisiert
+
+- ISY-775: Die beiden Readme-Dateien wurden für Standalone-Projekte aktualisiert
 - ISY-618: Eingabe eines Punktes an Cursor-Position 3 führt nun nicht mehr zur automatischen Vervollständigung des Monats in
   der Form 'xx'. Dies ermöglicht die Eingabe herkömmlicher Datumsformate mit Punkten, wie zum Beispiel '01.01.2024'
-## Features 
+- ISY-905: Linksnavigation und Informationsbereich können jetzt wieder korrekt eingeklappt werden und die Breite der Seitenleisten liegt nun standardmäßig wieder bei 15em
+## Features
 - ISY-756: IsyFact-Theme wurde überarbeitet
-- ISY-630: Das HTML lang-Attribut passt sich der gewählten Sprache automatisch an um die Zugänglichkeit der Website zu verbessern
-- ISY-682: Pipe 'IncompleteDatePipe' zur Anzeige von ungewissen Datum Werten wurde hinzugefügt
+- ISY-630: Das HTML lang-Attribut passt sich der gewählten Sprache automatisch an, um die Zugänglichkeit der Website zu verbessern
+- ISY-682: Pipe 'IncompleteDatePipe' zur Anzeige von ungewissen Datumswerten wurde hinzugefügt
 ## Demo-Anwendung
 - ISY-647: Alle Formulare wurden von rechtsbündigen Labels auf Floating-Labels umgestellt
 - ISY-634: Formulare wurden auf den Seiten _Objekt suchen_ und _Objekt-Anzeigen_ für verbesserte Responsivität überarbeitet
-- ISY-389: Imports in der Demo-Anwendung zur Library geht nun über den Library-Namen 
+- ISY-389: Imports in der Demo-Anwendung zur Library geht nun über den Library-Namen
 
 # 17.1.0 - 18.01.2024
 ## Fixes
--- projects/isy-angular-widgets/src/lib/hauptfenster/hauptfenster.component.spec.ts
+++ projects/isy-angular-widgets/src/lib/hauptfenster/hauptfenster.component.spec.ts
@@ -1,5 +1,5 @@
 import {HauptfensterComponent} from './hauptfenster.component';
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {createComponentFactory, Spectator} from '@ngneat/spectator';
 import {MegaMenu, MegaMenuSub} from 'primeng/megamenu';
 import {Button} from 'primeng/button';
@@ -16,7 +16,7 @@ import {HauptfensterModule} from './hauptfenster.module';
   `
 })
 class HauptFensterWrapperComponent {
-  @Input() title!: string;
+  title!: string;
 }
 
 describe('Unit Tests: HauptfensterComponent', () => {
@@ -81,6 +81,44 @@ describe('Unit Tests: HauptfensterComponent', () => {
     spectator.fixture.detectChanges();
     const informationsbereich = spectator.query('.isy-hauptfenster-informationsbereich') as HTMLElement;
     expect(informationsbereich.style.width).toEqual(customInformationsbereichWidth);
+  });
+
+  it('should not change its Linksnavigation width when collapsed', () => {
+    spectator.component.showLinksnavigation = true;
+    spectator.fixture.detectChanges();
+    const linksnavigation = spectator.query('.isy-hauptfenster-linksnavigation') as HTMLElement;
+    (linksnavigation.querySelector('.collapseButton button') as HTMLElement).click();
+    spectator.fixture.detectChanges();
+    const width = linksnavigation.style.width;
+
+    spectator.component.linksNavigationWidth = '20%';
+    spectator.fixture.detectChanges();
+
+    expect(linksnavigation.style.width).toEqual(width);
+
+    spectator.component.linksNavigationWidth = '10%';
+    spectator.fixture.detectChanges();
+
+    expect(linksnavigation.style.width).toEqual(width);
+  });
+
+  it('should not change its Informationsbereich width when collapsed', () => {
+    spectator.component.showInformationsbereich = true;
+    spectator.fixture.detectChanges();
+    const informationsbereich = spectator.query('.isy-hauptfenster-informationsbereich') as HTMLElement;
+    (informationsbereich.querySelector('.collapseButton button') as HTMLElement).click();
+    spectator.fixture.detectChanges();
+    const width = informationsbereich.style.width;
+
+    spectator.component.linksNavigationWidth = '20%';
+    spectator.fixture.detectChanges();
+
+    expect(informationsbereich.style.width).toEqual(width);
+
+    spectator.component.linksNavigationWidth = '10%';
+    spectator.fixture.detectChanges();
+
+    expect(informationsbereich.style.width).toEqual(width);
   });
 });
 
