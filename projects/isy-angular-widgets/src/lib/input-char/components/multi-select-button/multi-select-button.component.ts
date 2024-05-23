e9b6a6e01dc14d7421df5f204a7f226db5897975
Thao Pham <info@mtpham.de>
2024-05-23 11:17:48 +0200

    IFS-3605: Test and comments updated

--- projects/isy-angular-widgets/src/lib/input-char/components/multi-select-button/multi-select-button.component.ts
+++ projects/isy-angular-widgets/src/lib/input-char/components/multi-select-button/multi-select-button.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 import {CommonModule} from '@angular/common'; // Import CommonModule for common directives
 import {ControlValueAccessor, FormsModule} from '@angular/forms'; // Import FormsModule if you're using template-driven forms
 import {InputCharData} from '../../model/model';
