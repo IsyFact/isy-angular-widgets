a0d32c2b9dc1ecf2c66d6202b2089e255d9f2d65
strenj <julia.strenge@msg.group>
2024-09-02 11:44:21 +0200

    feat: IFS-4054 Widgetdivider added

--- projects/isy-angular-widgets-demo/src/app/feature/primeng-widgets/primeng-widgets.module.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/primeng-widgets/primeng-widgets.module.ts
@@ -1,12 +1,14 @@
 import {PrimengWidgetsRoutingModule} from './primeng-widgets-routing.module';
 import {PrimengWidgetsComponent} from './primeng-widgets.component';
 import {NgModule} from '@angular/core';
+import {TabViewModule} from 'primeng/tabview';
 
 @NgModule({
   declarations: [PrimengWidgetsComponent],
   providers: [],
   imports: [
-    PrimengWidgetsRoutingModule
+    PrimengWidgetsRoutingModule,
+    TabViewModule
   ]
 })
 export class PrimengWidgetsModule {}
