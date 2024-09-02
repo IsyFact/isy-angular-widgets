d0f8404bf5e9eba601544184d9152648195144f2
strenj <julia.strenge@msg.group>
2024-09-02 12:31:42 +0200

    feat: IFS-4054 Add widgets calender and checkbox

--- projects/isy-angular-widgets-demo/src/app/feature/primeng-widgets/primeng-widgets.module.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/primeng-widgets/primeng-widgets.module.ts
@@ -2,13 +2,17 @@ import {PrimengWidgetsRoutingModule} from './primeng-widgets-routing.module';
 import {PrimengWidgetsComponent} from './primeng-widgets.component';
 import {NgModule} from '@angular/core';
 import {TabViewModule} from 'primeng/tabview';
+import {CalendarModule} from 'primeng/calendar';
+import {CheckboxModule} from 'primeng/checkbox';
 
 @NgModule({
   declarations: [PrimengWidgetsComponent],
   providers: [],
   imports: [
     PrimengWidgetsRoutingModule,
-    TabViewModule
+    TabViewModule,
+    CalendarModule,
+    CheckboxModule
   ]
 })
 export class PrimengWidgetsModule {}
