87758d62aed2fdbae65fb4f79a94abad72b8b46a
strenj <julia.strenge@msg.group>
2024-09-09 13:50:02 +0200

    feat: IFS-4054 Add fieldset, panel and splitter widgets

--- projects/isy-angular-widgets-demo/src/app/feature/primeng-widgets/primeng-widgets.module.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/primeng-widgets/primeng-widgets.module.ts
@@ -45,6 +45,9 @@ import {TreeTableModule} from 'primeng/treetable';
 import {AccordionModule} from 'primeng/accordion';
 import {CardModule} from 'primeng/card';
 import {DividerModule} from 'primeng/divider';
+import {FieldsetModule} from 'primeng/fieldset';
+import {PanelModule} from 'primeng/panel';
+import {SplitterModule} from 'primeng/splitter';
 
 @NgModule({
   declarations: [PrimengWidgetsComponent],
@@ -94,7 +97,10 @@ import {DividerModule} from 'primeng/divider';
     TreeTableModule,
     AccordionModule,
     CardModule,
-    DividerModule
+    DividerModule,
+    FieldsetModule,
+    PanelModule,
+    SplitterModule
   ]
 })
 export class PrimengWidgetsModule {}
