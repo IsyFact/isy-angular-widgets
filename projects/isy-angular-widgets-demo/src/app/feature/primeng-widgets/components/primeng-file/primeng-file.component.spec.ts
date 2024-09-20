27613fd17b343cdbc4eece26a9d56bed20fb7314
strenj <julia.strenge@msg.group>
2024-09-20 12:17:12 +0200

    feat: IFS-4054 Add tests to primeng-file

--- projects/isy-angular-widgets-demo/src/app/feature/primeng-widgets/components/primeng-file/primeng-file.component.spec.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/primeng-widgets/components/primeng-file/primeng-file.component.spec.ts
@@ -1,14 +1,17 @@
 import {createComponentFactory, Spectator} from '@ngneat/spectator';
-
 import {PrimengFileComponent} from './primeng-file.component';
 import {PrimengWidgetsModule} from '../../primeng-widgets.module';
+import {UploadEvent} from 'primeng/fileupload';
+import {MessageService} from 'primeng/api';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 
 describe('Unit Tests: PrimengFileComponent', () => {
   let component: PrimengFileComponent;
   let spectator: Spectator<PrimengFileComponent>;
   const createComponent = createComponentFactory({
     component: PrimengFileComponent,
-    imports: [PrimengWidgetsModule]
+    imports: [PrimengWidgetsModule],
+    providers: [MessageService, provideHttpClient(withInterceptorsFromDi())]
   });
 
   beforeEach(() => {
@@ -18,5 +21,14 @@ describe('Unit Tests: PrimengFileComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add success message on file upload', () => {
+    const addMessageSpy = spyOn(component.messageService, 'add');
+    const mockEvent = {files: []} as unknown as UploadEvent;
+
+    component.onUpload(mockEvent);
+
+    expect(addMessageSpy).toHaveBeenCalledWith({severity: 'info', summary: 'Success', detail: 'File Uploaded with Basic Mode'});
   });
 });
