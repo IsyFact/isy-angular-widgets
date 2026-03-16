23e9543961b942dda642bcc4eb214edcfbefb2a5
Niklas Clemens <Niklas.Clemens.ext@msg.group>
2026-03-16 18:11:07 +0100

    feat: IFS-4999 added tests for incomplete-date.component.spec.ts and isy-angular-components.component.spec.ts

--- projects/isy-angular-widgets-demo/src/app/feature/isy-angular-components/isy-angular-components.component.spec.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/isy-angular-components/isy-angular-components.component.spec.ts
@@ -1,6 +1,7 @@
 import {createComponentFactory, Spectator} from '@ngneat/spectator';
 import {IsyAngularComponentsComponent} from './isy-angular-components.component';
 import {TranslateModule} from '@ngx-translate/core';
+import {IncompleteDateComponent} from '@isy-angular-widgets/incomplete-date/incomplete-date.component';
 
 describe('IsyAngularComponentsComponent', () => {
   let spectator: Spectator<IsyAngularComponentsComponent>;
@@ -15,5 +16,25 @@ describe('IsyAngularComponentsComponent', () => {
 
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
+  });
+
+  it('should initialize transferDateAsIso8601 with true', () => {
+    expect(spectator.component.transferDateAsIso8601).toBeTrue();
+  });
+
+  it('should call updateModel and updateValueAndValidity when transfer mode changes', () => {
+    const incompleteDateComponent = jasmine.createSpyObj<IncompleteDateComponent>('IncompleteDateComponent', [
+      'updateModel'
+    ]);
+
+    const updateValueAndValiditySpy = spyOn(
+      spectator.component.personalInfoForm.controls.dateOfEntry,
+      'updateValueAndValidity'
+    );
+
+    spectator.component.onTransferIso8601Change(incompleteDateComponent);
+
+    expect(incompleteDateComponent.updateModel).toHaveBeenCalled();
+    expect(updateValueAndValiditySpy).toHaveBeenCalled();
   });
 });
