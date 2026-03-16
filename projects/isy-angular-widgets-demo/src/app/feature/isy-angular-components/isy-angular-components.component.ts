6feef7a015c0a0a384b0dd71c132b38639a3a5d0
Niklas Clemens <Niklas.Clemens.ext@msg.group>
2026-03-16 17:24:20 +0100

    feat: IFS-4999 improve incomplete-date demo for ISO 8601 toggle and validation feedback

--- projects/isy-angular-widgets-demo/src/app/feature/isy-angular-components/isy-angular-components.component.ts
+++ projects/isy-angular-widgets-demo/src/app/feature/isy-angular-components/isy-angular-components.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators, FormsModule} from '@angular/forms';
 import {FormWrapperComponent} from '@isy-angular-widgets/form-wrapper/form-wrapper.component';
 import {TranslateModule} from '@ngx-translate/core';
 import {FormControlPipe} from '@isy-angular-widgets/pipes/form-control.pipe';
@@ -9,14 +9,14 @@ import {initializedPerson} from '../objekt-anzeigen/data';
 import {InputCharDirective} from '@isy-angular-widgets/input-char/directives/input-char.directive';
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
-import { JsonPipe } from '@angular/common';
+import {CheckboxModule} from 'primeng/checkbox';
 
 @Component({
   standalone: true,
   selector: 'demo-isy-angular-components',
   templateUrl: './isy-angular-components.component.html',
   imports: [
-    JsonPipe,
+    FormsModule,
     FormWrapperComponent,
     TranslateModule,
     FormControlPipe,
@@ -25,13 +25,16 @@ import { JsonPipe } from '@angular/common';
     InputCharDirective,
     InputTextModule,
     SeitentoolbarComponent,
-    ButtonModule
+    ButtonModule,
+    CheckboxModule
   ]
 })
 export class IsyAngularComponentsComponent {
   personalInfoForm: FormGroup;
   person = initializedPerson;
   personalien = this.person.personalien;
+
+  transferDateAsIso8601 = true;
 
   private readonly fb = inject(FormBuilder);
 
@@ -41,5 +44,10 @@ export class IsyAngularComponentsComponent {
       lastName: [this.personalien.nachname, Validators.required],
       dateOfEntry: [this.personalien.einreisedatum]
     });
+  }
+
+  onTransferIso8601Change(incompleteDateComponent: IncompleteDateComponent): void {
+    incompleteDateComponent.updateModel();
+    this.personalInfoForm.controls.dateOfEntry.updateValueAndValidity();
   }
 }
